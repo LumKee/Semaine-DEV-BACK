063d0fb49a0a62408004da648075897f1f7bddd1
UlysseGebe <Uly35534e31>
2019-04-11 02:38:55 +0200

    materiels

---
+++ views/templates/Materiels.php
@@ -0,0 +1,144 @@
+<div class="besoin">
+    <div class="title">
+        <h2>Besoin</h2>
+        <div class="move"></div>
+    </div>
+    <div class="itemList">
+        <div class="container">Produit
+            <div class="move_1"></div>
+            <p>In order to discuss the general funct of the logo. </p>
+        </div>
+        <div class="container">Prod
+            <div class="move_1"></div>
+            <p>In order to discuss the general funct of the logo. </p>
+        </div>
+        <div class="container">Produit
+            <div class="move_1"></div>
+            <p>In order to discuss the general funct of the logo. </p>
+        </div>
+        <div class="container">Prod
+            <div class="move_1"></div>
+            <p>In order to discuss the general funct of the logo. </p>
+        </div>
+        <div class="container">Produit
+            <div class="move_1"></div>
+            <p>In order to discuss the general funct of the logo. </p>
+        </div>
+        <div class="container">Prod
+            <div class="move_1"></div>
+            <p>In order to discuss the general funct of the logo. </p>
+        </div>
+        <div class="container">Produit
+            <div class="move_1"></div>
+            <p>In order to discuss the general funct of the logo. </p>
+        </div>
+        <div class="container">Prod
+            <div class="move_1"></div>
+            <p>In order to discuss the general funct of the logo. </p>
+        </div>
+    </div>
+</div>
+<div class="achat">
+    <div class="title">
+        <h2>Achat</h2>
+        <div class="move"></div>
+    </div>
+    <div class="itemList">
+        <div class="container">Produit
+            <div class="move_1"></div>
+            <p>In order to discuss the general funct of the logo. </p>
+            <div class="users">
+                <div class="user_btn"></div>
+                <div class="user_perso"></div>
+            </div>
+        </div>
+        <div class="container">Prod
+            <div class="move_1"></div>
+            <p>In order to discuss the general funct of the logo. </p>
+            <div class="users">
+                <div class="user_btn"></div>
+                <div class="user_perso"></div>
+            </div>
+        </div>
+        <div class="container">Produit
+            <div class="move_1"></div>
+            <p>In order to discuss the general funct of the logo. </p>
+            <div class="users">
+                <div class="user_btn"></div>
+                <div class="user_perso"></div>
+            </div>
+        </div>
+        <div class="container">Prod
+            <div class="move_1"></div>
+            <p>In order to discuss the general funct of the logo. </p>
+            <div class="users">
+                <div class="user_btn"></div>
+                <div class="user_perso"></div>
+            </div>
+        </div>
+        <div class="container">Produit
+            <div class="move_1"></div>
+            <p>In order to discuss the general funct of the logo. </p>
+            <div class="users">
+                <div class="user_btn"></div>
+                <div class="user_perso"></div>
+            </div>
+        </div>
+        <div class="container">Prod
+            <div class="move_1"></div>
+            <p>In order to discuss the general funct of the logo. </p>
+            <div class="users">
+                <div class="user_btn"></div>
+                <div class="user_perso"></div>
+            </div>
+        </div>
+        <div class="container">Produit
+            <div class="move_1"></div>
+            <p>In order to discuss the general funct of the logo. </p>
+            <div class="users">
+                <div class="user_btn"></div>
+                <div class="user_perso"></div>
+            </div>
+        </div>
+        <div class="container">Prod
+            <div class="move_1"></div>
+            <p>In order to discuss the general funct of the logo. </p>
+            <div class="users">
+                <div class="user_btn"></div>
+                <div class="user_perso"></div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="acquis">
+    <div class="title">
+        <h2>Acquis</h2>
+        <div class="move"></div>
+    </div>
+    <div class="itemList">
+        <div class="container">Produit
+            <div class="move_1"></div>
+        </div>
+        <div class="container">Prod
+            <div class="move_1"></div>
+        </div>
+        <div class="container">Produit
+            <div class="move_1"></div>
+        </div>
+        <div class="container">Prod
+            <div class="move_1"></div>
+        </div>
+        <div class="container">Produit
+            <div class="move_1"></div>
+        </div>
+        <div class="container">Prod
+            <div class="move_1"></div>
+        </div>
+        <div class="container">Produit
+            <div class="move_1"></div>
+        </div>
+        <div class="container">Prod
+            <div class="move_1"></div>
+        </div>
+    </div>
+</div>
