b8a2683c609974391edc70768c421e3a17cc6abc
UlysseGebe <Uly35534e31>
2019-04-12 12:20:07 +0200

    correction idParty

--- views/pages/confirmation.php
+++ views/pages/confirmation.php
@@ -4,8 +4,9 @@
 <div class="container">
     <img class="illustration" src="<?= URL ?>assets/images/illustration.svg" alt="illustration" />
     <h2>Bravo ta soirée vient d’être créée !</h2>
+    <h2 class='give'>Ton Id de soirée: <?php echo $_SESSION['party_id'];?></h2></h2>
     <div class="btnwrap">
-        <div class="btn1"><a href="<?php URL ?>dashboard">Poursuivre</a></div>
+        <div class="btn1"><a href="<?php URL ?>dashboard/Nourriture">Poursuivre</a></div>
         <div class="btn2"><a href="#">Invite tes amis</a></div>
     </div>
 </div>
