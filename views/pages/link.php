0b0690a5565a164cd89f0f2fba8d75e0ce3b75f1
LumKee <33061575+LumKee@users.noreply.github.com>
2019-04-12 11:14:26 +0200

    Update link.php

--- views/pages/link.php
+++ views/pages/link.php
@@ -1,9 +1,21 @@
 <?php session_start(); ?>
+
 <?php include '../views/partials/head.php'; ?>
+
 <div class="container">
     <a href="<?= URL ?>"><img class="logo" src="../public/assets/images/logo.svg" alt="logo"></a>
+
     <img class="illustration" src="../public/assets/images/illustration.svg" alt="illustrations">
+
     <h1>Rejoint tes amis :</h1>
-    <input onClick="window.location.href='<?php URL ?>dashboard'" class="Btn" type="submit" value="Poursuivre">
+
+    <!-- <input onClick="window.location.href='dashboard'" class="Btn" type="submit" value="Poursuivre"> -->
+    
+    <!-- NOUVEAU BUTTON -->
+    <div class="button-4">
+        <div class="eff-4"></div>
+        <a href="<?php URL ?>dashboard">Poursuivre</a>
+    </div>
 </div>
+
 <?php include '../views/partials/footer.php';
