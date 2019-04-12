ed308b11538c118b20a9288601292899788a6a5e
LumKee <33061575+LumKee@users.noreply.github.com>
2019-04-12 11:14:05 +0200

    Update confirmation.php

--- views/pages/confirmation.php
+++ views/pages/confirmation.php
@@ -1,12 +1,24 @@
 <?php session_start() ?>
+
 <?php include '../views/partials/head.php'; ?>
+
 <a href="<?= URL ?>"><img class="logo" src="../public/assets/images/logo.svg" alt="logo"></a>
+
 <div class="container">
     <img class="illustration" src="../public/assets/images/illustration.svg" alt="illustration" />
+
     <h2>Bravo ta soirée vient d’être créée !</h2>
-    <div class="btnwrap">
-        <div class="btn1"><a href="<?php URL ?>dashboard">Poursuivre</a></div>
-        <div class="btn2"><a href="#">Invite tes amis</a></div>
+
+    <div class="button-4">
+        <div class="eff-4"></div>
+        <a href="<?php URL ?>dashboard">Poursuivre</a>
     </div>
+
+    <div class="button-5">
+        <div class="eff-5"></div>
+        <a href="<?php URL ?>dashboard">Inviter vos amis</a>
+    </div>
+
 </div>
+
 <?php include '../views/partials/footer.php';
